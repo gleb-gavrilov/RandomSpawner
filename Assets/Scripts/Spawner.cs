0115320835ecf9c29147ddb3bc5a6c340e424bc3
gleb-gavrilov <y1@bk.ru>
2022-10-30 20:11:09 +0300

    change coroutine call

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -9,27 +9,21 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float _delay;
 
     private int _randomSpawnPosition;
-    private Coroutine _generateEnemies;
     private WaitForSeconds _wait;
 
     private void Start()
     {
         _wait = new WaitForSeconds(_delay);
-    }
-
-    private void Update()
-    {
-        if (_generateEnemies == null)
-        {
-            _generateEnemies = StartCoroutine(GenerateEnemies());
-        }
+        StartCoroutine(GenerateEnemies());
     }
 
     private IEnumerator GenerateEnemies()
     {
-        _randomSpawnPosition = Random.Range(0, _spawnPoints.Length);
-        Instantiate(_enemy, _spawnPoints[_randomSpawnPosition].transform.position, Quaternion.identity);
-        yield return _wait;
-        _generateEnemies = null;
+        while(true)
+        {
+            _randomSpawnPosition = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_enemy, _spawnPoints[_randomSpawnPosition].transform.position, Quaternion.identity);
+            yield return _wait;
+        }
     }
 }
